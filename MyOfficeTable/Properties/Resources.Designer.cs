268c2446f213fbf125fe952ff013941d55764f55
Leron-D <shangin21072003@gmail.com>
2022-12-13 20:11:08 +0300

    changes

--- MyOfficeTable/Properties/Resources.Designer.cs
+++ MyOfficeTable/Properties/Resources.Designer.cs
@@ -81,6 +81,47 @@ namespace MyOfficeTable.Properties {
         }
         
         /// <summary>
+        ///   Ищет локализованную строку, похожую на &lt;html&gt;
+        ///&lt;meta http-equiv=&quot;X-UA-Compatible&quot; content=&quot;IE=edge&quot;/&gt;
+        ///&lt;head&gt;
+        ///	&lt;title&gt;МойОфис Таблица&lt;/title&gt;
+        ///	&lt;style&gt;
+        ///		body
+        ///		{
+        ///			font-size: 14pt
+        ///		}
+        ///		.headers
+        ///		{
+        ///			font-weight: bold;
+        ///			font-size: 22pt;
+        ///			list-style-type: upper-roman;
+        ///			
+        ///		}
+        ///		.subheaders
+        ///		{
+        ///			font-size: 18pt
+        ///		}
+        ///	&lt;/style&gt;
+        ///&lt;/head&gt;
+        ///&lt;body&gt;
+        ///	&lt;h1&gt;
+        ///	&lt;center&gt;
+        ///	&lt;div&gt;
+        ///		&lt;div&gt;&lt;img src=&apos;MyOfficeTable.jpg&apos; height=&apos;15%&apos;/&gt;&lt;/div&gt;
+        ///		&lt;div&gt;МойОфис Таблица&lt;/div&gt;
+        ///	&lt;/center&gt;
+        ///	&lt;/h1&gt;
+        ///	&lt;br&gt;
+        ///	&lt;ol&gt;
+        ///		&lt;li class=&apos;headers&apos;&gt;Работа в пр [остаток строки не уместился]&quot;;.
+        /// </summary>
+        internal static string Lection {
+            get {
+                return ResourceManager.GetString("Lection", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap MyOfficeTable {
@@ -103,10 +144,243 @@ namespace MyOfficeTable.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap MyOfficeTable2 {
+            get {
+                object obj = ResourceManager.GetObject("MyOfficeTable2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture1 {
+            get {
+                object obj = ResourceManager.GetObject("picture1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture10 {
+            get {
+                object obj = ResourceManager.GetObject("picture10", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture11 {
+            get {
+                object obj = ResourceManager.GetObject("picture11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture12 {
+            get {
+                object obj = ResourceManager.GetObject("picture12", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture13 {
+            get {
+                object obj = ResourceManager.GetObject("picture13", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture14 {
+            get {
+                object obj = ResourceManager.GetObject("picture14", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture15 {
+            get {
+                object obj = ResourceManager.GetObject("picture15", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture16 {
+            get {
+                object obj = ResourceManager.GetObject("picture16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture17 {
+            get {
+                object obj = ResourceManager.GetObject("picture17", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture18 {
+            get {
+                object obj = ResourceManager.GetObject("picture18", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture19 {
+            get {
+                object obj = ResourceManager.GetObject("picture19", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture2 {
+            get {
+                object obj = ResourceManager.GetObject("picture2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture20 {
+            get {
+                object obj = ResourceManager.GetObject("picture20", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture3 {
+            get {
+                object obj = ResourceManager.GetObject("picture3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture4 {
+            get {
+                object obj = ResourceManager.GetObject("picture4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture5 {
+            get {
+                object obj = ResourceManager.GetObject("picture5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture6 {
+            get {
+                object obj = ResourceManager.GetObject("picture6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture7 {
+            get {
+                object obj = ResourceManager.GetObject("picture7", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture8 {
+            get {
+                object obj = ResourceManager.GetObject("picture8", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap picture9 {
+            get {
+                object obj = ResourceManager.GetObject("picture9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap question {
             get {
                 object obj = ResourceManager.GetObject("question", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap savePict {
+            get {
+                object obj = ResourceManager.GetObject("savePict", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Ищет локализованную строку, похожую на &lt;Array&gt;
+        ///	&lt;Question Type=&quot;String&quot; Text=&quot;Что будет указано в строке заголовка, если документ был изменён и не проводилось сохранение?&quot;	True=&quot;изменён&quot;&gt;&lt;/Question&gt;
+        ///	&lt;Question Type=&quot;String&quot; Text=&quot;Что расположено под командным меню?&quot; True=&quot;панель инструментов&quot;&gt;&lt;/Question&gt;
+        ///	&lt;Question Type=&quot;String&quot; Text=&quot;Как называется меню, содержащее команды для работы с файлами?&quot; True=&quot;файл&quot;&gt;&lt;/Question&gt;
+        ///	&lt;Question Type=&quot;String&quot; Text=&quot;Какая команда будет выполнена, если навести курсор мыши на границу раздела и нажать на стрел [остаток строки не уместился]&quot;;.
+        /// </summary>
+        internal static string Работа_в_приложении_МойОфис_Таблица {
+            get {
+                return ResourceManager.GetString("Работа_в_приложении_МойОфис_Таблица", resourceCulture);
             }
         }
     }
